937a78c56f9b0c86b68394284dd4e6c0cd9b9bd9
RinFiz <121906192+RinFiz@users.noreply.github.com>
2023-05-29 19:35:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,14 @@ const dropdown = document.querySelector('.dropdown a');
 
 dropdown.addEventListener('click', (event) => {
   event.preventDefault();
-
-  const text = dropdown.querySelector('strong').textContent.trim();
-
-  const tempTextarea = document.createElement('textarea');
-  tempTextarea.value = text;
-  document.body.appendChild(tempTextarea);
-
-  tempTextarea.select();
-  tempTextarea.setSelectionRange(0, 99999);
-
-  document.execCommand('copy');
-  document.body.removeChild(tempTextarea);
-
-  alert(`"${text}" has been copied to the clipboard.`);
+  
+  const text = dropdown.innerText.trim();
+  
+  navigator.clipboard.writeText(text)
+    .then(() => {
+      alert(`"${text}" has been copied to the clipboard.`);
+    })
+    .catch((error) => {
+      console.error(`Unable to copy text: ${error}`);
+    });
 });
