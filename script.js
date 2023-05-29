89c89c7be6a4f2814d08817fb8e7f771053b63fc
RinFiz <121906192+RinFiz@users.noreply.github.com>
2023-05-29 19:36:59 -0400

    still trying to fix copy and paste feature

--- script.js
+++ script.js
@@ -2,14 +2,25 @@ const dropdown = document.querySelector('.dropdown a');
 
 dropdown.addEventListener('click', (event) => {
   event.preventDefault();
-  
-  const text = dropdown.innerText.trim();
-  
-  navigator.clipboard.writeText(text)
-    .then(() => {
+
+  const text = dropdown.querySelector('strong').textContent.trim();
+
+  const tempTextarea = document.createElement('textarea');
+  tempTextarea.value = text;
+  document.body.appendChild(tempTextarea);
+
+  tempTextarea.select();
+
+  try {
+    const successful = document.execCommand('copy');
+    if (successful) {
       alert(`"${text}" has been copied to the clipboard.`);
-    })
-    .catch((error) => {
-      console.error(`Unable to copy text: ${error}`);
-    });
+    } else {
+      throw new Error('Copy command unsuccessful.');
+    }
+  } catch (error) {
+    console.error(`Unable to copy text: ${error}`);
+  }
+
+  document.body.removeChild(tempTextarea);
 });
